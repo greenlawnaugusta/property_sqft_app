80464bf719ba8007c8c6cae93f328b6891cb7073
greenlawnaugusta <greenlawnaugusta@gmail.com>
2024-12-26 01:19:41 -0500

    Update property_sqft_app.py

--- property_sqft_app.py
+++ property_sqft_app.py
@@ -216,9 +216,9 @@ def create_checkout_session():
             }],
             mode='payment',
             customer=customer.id,
-            success_url='	
-https://pricing.greenlawnaugusta.com/home-page?contact_id={contact_id}&first_name={first_name}&last_name={last_name}&email={email}&phone={phone}&address={address}&turf_sq_ft={turf_sq_ft}&recurring_maintenance_biweekly_price={recurring_maintenance_biweekly_price}&recurring_maintenance_weekly_price={recurring_maintenance_weekly_price}&one_time_mow_price={one_time_mow_price}&full_service_biweekly_price={full_service_biweekly_price}&full_service_weekly_price={full_service_weekly_price}&weed_control_1_price={weed_control_1_price}&weed_control_2_price={weed_control_2_price}&weed_control_3_price={weed_control_3_price}',
+            success_url='https://yourdomain.com/success',
             cancel_url='https://yourdomain.com/cancel',
+
         )
 
         # Generate Trigger Link
